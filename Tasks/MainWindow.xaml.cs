45f6e54ebdfc8013a4399de5bb7814d64933169d
vladkovalevich <64867756+vladkovalevich@users.noreply.github.com>
2020-05-14 17:46:52 +0300

    Добавьте файлы проекта.

---
+++ Tasks/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tasks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void _Task1_Click(object sender, RoutedEventArgs e)
+        {
+            string[] A = _InputStr.Text.Split(' ');
+            // int Resault = Convert.ToInt32(A);
+            foreach (string Resault in A)
+            {
+                _OutputStr.Text = "Ответ"+Convert.ToString(Resault);
+            }
+            
+        }
+    }
+}
